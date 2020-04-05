fe66d84318526586fefa86457cc168710da81a52
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-04-05 17:02:11 +0300

    Updates

---
+++ precache-manifest.fc4acf6f38c3c95a5cb3233d8d6581b3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2bf6344e81a48abd3689990fe26a69",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e4d993df341714f86cb8",
+    "url": "/Portfolio/static/css/main.3caf5924.chunk.css"
+  },
+  {
+    "revision": "a05768caf4d228e4c290",
+    "url": "/Portfolio/static/js/2.0c01adda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.0c01adda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d993df341714f86cb8",
+    "url": "/Portfolio/static/js/main.93936afa.chunk.js"
+  },
+  {
+    "revision": "43f1dfbd388c45f57593",
+    "url": "/Portfolio/static/js/runtime-main.2445a1df.js"
+  },
+  {
+    "revision": "fcedbf22e17f156e82599e4f3c3309f4",
+    "url": "/Portfolio/static/media/css3.fcedbf22.svg"
+  },
+  {
+    "revision": "b5877fd874abdd4b06d65421dad8d914",
+    "url": "/Portfolio/static/media/facebook.b5877fd8.svg"
+  },
+  {
+    "revision": "072cde1bbbaedd886f95ce17a6a364a4",
+    "url": "/Portfolio/static/media/github.072cde1b.svg"
+  },
+  {
+    "revision": "3db2ec8c90a1a64d3e2304f0abb764cc",
+    "url": "/Portfolio/static/media/html-5.3db2ec8c.svg"
+  },
+  {
+    "revision": "1f9d2a03d052f83673fe942c47811daf",
+    "url": "/Portfolio/static/media/javascript.1f9d2a03.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/Portfolio/static/media/linkedin.d849bc25.svg"
+  },
+  {
+    "revision": "22a4a50b0638a729b01fbbd7ca3ed5c5",
+    "url": "/Portfolio/static/media/network.22a4a50b.png"
+  },
+  {
+    "revision": "fa8e12f52f0214a27ae7ab257a7c1db9",
+    "url": "/Portfolio/static/media/omni.fa8e12f5.png"
+  },
+  {
+    "revision": "d9a5f23e8341184d27a06cd8448835e5",
+    "url": "/Portfolio/static/media/portfoliophoto.d9a5f23e.jpg"
+  },
+  {
+    "revision": "eae78e809fcfe9799d89753d05234867",
+    "url": "/Portfolio/static/media/react-native.eae78e80.svg"
+  },
+  {
+    "revision": "5d7e10d2fcbf8e80ddd4123d6a2affa7",
+    "url": "/Portfolio/static/media/redux.5d7e10d2.svg"
+  },
+  {
+    "revision": "aee5a0d2e7ad1917df1aca79b0fd2d1f",
+    "url": "/Portfolio/static/media/shop.aee5a0d2.png"
+  }
+]);
