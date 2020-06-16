558a46c5637027ccb2f2c94e9d5bbea615b91b65
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-06-16 16:48:02 +0300

    add scrolling

--- src/components/header/NavMenu/Menu.tsx
+++ src/components/header/NavMenu/Menu.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from "./NavMenu.module.css";
+import {Link, animateScroll as scroll} from "react-scroll";
 
 const Menu = (props: any) => {
     return <div className={styles.n}>
         <ul className={styles.nav}>
-            <li className={styles.link}><a href='#Main'>Main</a></li>
-            <li className={styles.link}><a href='#Skills'>Skills</a></li>
-            <li className={styles.link}><a href='#Projects'>Projects</a></li>
-            <li className={styles.link}><a href='#Contacts'>Contacts</a></li>
+            <Link activeClass="active" to="Main" spy={true} smooth={true} offset={-70} duration={500}
+                  className={styles.link}><a href='#Main'>Main</a></Link>
+            <Link activeClass="active" to="Skills" spy={true} smooth={true} offset={-70} duration={500}
+                  className={styles.link}>
+                <a href='#Skills'>Skills</a></Link>
+            <Link activeClass="active" to="Projects" spy={true} smooth={true} offset={-70} duration={500}
+                  className={styles.link}><a href='#Projects'>Projects</a></Link>
+            <Link activeClass="active" to="Contacts" spy={true} smooth={true} offset={-70} duration={500}
+                  className={styles.link}><a href='#Contacts'>Contacts</a></Link>
         </ul>
         <div className={styles.high} onClick={props.highNavMenu}>x</div>
     </div>
 }
-    
+
 export default Menu
